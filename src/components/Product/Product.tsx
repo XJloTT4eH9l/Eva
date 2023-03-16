7da510e33f7c8de3be834665cd8c536a837139c5
XJloTT4eH9l <andreyrekagg@gmail.com>
2023-03-16 22:56:47 +0200

    created product component

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import './Product.scss';
+
+interface ProductProps {
+    id: number;
+    title: string;
+    img: string;
+    price: number;
+}
+
+const Product:FC<ProductProps> = ({ id, title, img, price}) => {
+    return (
+        <Link 
+            key={id}
+            className='product' 
+            to={`/catalog/:${id}`}
+        >
+            <div className='product__top'>
+                <img className='product__img' src={img} alt={title} />
+                <h3 className='product__title'>{title}</h3>
+            </div>
+            <div className='product__bottom'>
+                <p className='product__price'>{price} грн</p>
+            </div>
+        </Link>
+    )
+}
+
+export default Product;
--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -1,5 +1,7 @@
 import Hero from '../../components/Hero/Hero';
 import Categories from '../../components/Categories/Categories';
+import Discount from '../../components/Discount/Discount';
+
 import './HomePage.scss';
 
 const HomePage = () => {
@@ -7,6 +9,7 @@ const HomePage = () => {
         <>
             <Hero /> 
             <Categories />
+            <Discount />
         </>
     )
 }
